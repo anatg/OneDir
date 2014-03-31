9c3e31095897ae955ad15db3536d7d72401bcc69
Casey Silver <caseyleesilver@gmail.com>
2014-03-30 20:18:45 -0400

    Associated uploaded files with logged in users

--- mysite/file_demo/admin.py
+++ mysite/file_demo/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from file_demo.models import Data
+from file_demo.models import Data, UserFiles
 
 # Register your models here.
 admin.site.register(Data)
+admin.site.register(UserFiles)
--- mysite/file_demo/models.py
+++ mysite/file_demo/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.core.files.storage import FileSystemStorage
 
 fs = FileSystemStorage()
@@ -8,3 +9,15 @@ class Data(models.Model):
     file = models.FileField(upload_to='.', storage=fs, blank=False, null=False)
     def __str__(self):
         return self.file.name
+
+#TODO: Add another way to give this function a directory structure
+def file(self, filename):
+    url = "users/%s/%s" % (self.user.username, filename)
+    return url
+
+#TODO: Figure out how to pass directory structure from view to file function here
+class UserFiles(models.Model):
+    user = models.ForeignKey(User)
+    file = models.FileField(upload_to=file, storage=fs, blank=False, null=False)
+    def __str__(self):
+        return 'User: ' + self.user.username + ', file: ' + self.file.name
