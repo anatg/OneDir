9aed8b06903ec62a361c9dd706514d432bee12ee
alepnull <ag3pk@virginia.edu>
2014-04-28 21:08:43 -0400

    script is working

---
+++ monitor/script.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo $(whoami)
+function move_files {
+	echo "move_files"
+	#mv client.py file_watcher.py transfer_manager.py /home/$(whoami)/OneDir/monitor/
+}
+function run {
+	#python client.py 
+	echo "running"
+}
+
+if [ -d /home/$(whoami)/OneDir/ ]
+then
+	if [ -d /home/$(whoami)/OneDir/monitor/ ]
+	then
+		if [ -e client.py -a -e file_watcher.py -a -e transfer_manager.py ]
+		then
+			echo "we're here"
+			run
+
+		else
+			move_files
+			run
+			
+		fi
+	else
+		mkdir "/home/$(whoami)/OneDir/monitor/"
+		move_files
+		run	
+	fi
+else
+	mkdir "/home/$(whoami)/OneDir/"
+	mkdir "/home/$(whoami)/OneDir/monitor/"
+	move_files
+	run	
+fi
+
+#python client.py
